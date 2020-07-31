ddf15561785da2512ed56aeeba6a519cddc5b6fe
natke <nkershaw@gmail.com>
2020-07-31 18:20:46 +0000

    Add ONNX model export from training code

--- getting-started/train_ort.py
+++ getting-started/train_ort.py
@@ -3,11 +3,9 @@ import math
 import torch
 import torch.nn as nn
 import torchtext
-from model import TransformerModel
 from torchtext.data.utils import get_tokenizer
-
+from model import TransformerModel
 from onnxruntime.capi.ort_trainer import IODescription, ModelDescription, ORTTrainer
-
 
 basic_english_tokenizer=get_tokenizer("basic_english")
 
@@ -18,8 +16,8 @@ TEXT = torchtext.data.Field(tokenize=basic_english_tokenizer,
 
 train_txt, val_txt, test_txt = torchtext.datasets.WikiText2.splits(TEXT)
 TEXT.build_vocab(train_txt)
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f'Device: {device}')
 
 bptt = 35
 def get_batch(source, i):
@@ -39,48 +37,38 @@ def batchify(data, bsz):
     return data.to(device)
 
 batch_size = 20
-#eval_batch_size = 10
 eval_batch_size = 20
 train_data = batchify(train_txt, batch_size)
 val_data = batchify(val_txt, eval_batch_size)
 test_data = batchify(test_txt, eval_batch_size)
 
 ntokens = len(TEXT.vocab.stoi) # the size of vocabulary
-emsize = 200 # embedding dimension
-nhid = 200 # the dimension of the feedforward network model in nn.TransformerEncoder
-nlayers = 2 # the number of nn.TransformerEncoderLayer in nn.TransformerEncoder
-nhead = 2 # the number of heads in the multiheadattention models
-dropout = 0.2 # the dropout value
+emsize = 200     # embedding dimension
+nhid = 200       # the dimension of the feedforward network model in nn.TransformerEncoder
+nlayers = 2      # the number of nn.TransformerEncoderLayer in nn.TransformerEncoder
+nhead = 2        # the number of heads in the multiheadattention models
+dropout = 0.2    # the dropout value
+
 model = TransformerModel(ntokens, emsize, nhead, nhid, nlayers, dropout).to(device)
 
 criterion = nn.CrossEntropyLoss()
 
-def flat_output_loss(output, target):
+def loss_with_flat_output(output, target):
     output = output.view(-1, ntokens)
     return criterion(output, target)
     
-#optimizer = torch.optim.SGD(model.parameters(), lr=lr)
-#lr = 5.0 # learning rate
 lr = 0.01 # learning rate
-#scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
 
 def train():
-    #model.train() # Turn on the train mode
     total_loss = 0.
     start_time = time.time()
-    #ntokens = len(TEXT.vocab.stoi)
     for batch, i in enumerate(range(0, train_data.size(0) - 1, bptt)):
         data, targets = get_batch(train_data, i)
         if len(data) < bptt:
             print(f"len(data)={len(data)} < {bptt}")
             continue
-        #optimizer.zero_grad()
-        loss = trainer.train_step(data, targets, torch.tensor([lr]))
-        #output = model(data)
-        #loss = criterion(output.view(-1, ntokens), targets)
-        #loss.backward()
-        #torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
-        #optimizer.step()
+        results = trainer.train_step(data, targets, torch.tensor([lr]))
+        loss = results[0]
 
         total_loss += loss.item()
         log_interval = 200
@@ -90,49 +78,42 @@ def train():
             print('| epoch {:3d} | {:5d}/{:5d} batches | '
                   'lr {:02.2f} | ms/batch {:5.2f} | '
                   'loss {:5.2f} | ppl {:8.2f}'.format(
-#                   epoch, batch, len(train_data) // bptt, scheduler.get_last_lr()[0],
                     epoch, batch, len(train_data) // bptt, lr,
                     elapsed * 1000 / log_interval,
-#                   cur_loss, math.exp(cur_loss)))
                     loss, math.exp(loss)))
             total_loss = 0
             start_time = time.time()
 
 def evaluate(eval_model, data_source):
-#   eval_model.eval() # Turn on the evaluation mode
     total_loss = 0.
-#   ntokens = len(TEXT.vocab.stoi)
     with torch.no_grad():
         for i in range(0, data_source.size(0) - 1, bptt):
             data, targets = get_batch(data_source, i)
             if len(data) < bptt:
                 print(f"len(data)={len(data)} < {bptt}")
                 continue
-            #output = eval_step(data)
-            #output_flat = output.view(-1, ntokens)
-            #total_loss += len(data) * criterion(output_flat, targets).item()
-            loss = trainer.eval_step(data, targets)
+            results = trainer.eval_step(data, targets)
+            loss = results[0]
             total_loss += len(data) * loss.item()
     return total_loss / (len(data_source) - 1)
 
-
 best_val_loss = float("inf")
 epochs = 3 # The number of epochs
-#best_model = None
-
+best_model = None
 
 def model_description():
     input_desc = IODescription('src', [bptt, batch_size], torch.float32)
     label_desc = IODescription('label', [bptt, batch_size, ntokens], torch.int64)
     loss_desc = IODescription('loss', [], torch.float32)
-    return ModelDescription([input_desc, label_desc], [loss_desc])
+    output_desc = IODescription('output', [bptt, batch_size, ntokens], torch.float32)
+    return ModelDescription([input_desc, label_desc], [loss_desc, output_desc])
 
 def learning_rate_description():
     return IODescription('Learning_Rate', [lr,], torch.float32)
 
 
 trainer = ORTTrainer(model,                       # model
-                     flat_output_loss,            # loss function
+                     loss_with_flat_output,       # loss function
                      model_description(),         # model description
                      "SGDOptimizer",              # optimizer name
                      None,                        # optimizer attributes
@@ -155,7 +136,6 @@ for epoch in range(1, epochs + 1):
         best_val_loss = val_loss
         best_model = model
 
-    #scheduler.step()
 
 test_loss = evaluate(best_model, test_data)
 print('=' * 89)
@@ -164,6 +144,22 @@ print('| End of training | test loss {:5.2f} | test ppl {:8.2f}'.format(
 print('=' * 89)
 
 
+# Load trained weighta into the PyTorch model
+model.load_state_dict(trainer.state_dict())
+
+# Generate input for model export 
+example_sentence = "Example input sentence"
+tokenized_sentence = basic_english_tokenizer(example_sentence)
+src = TEXT.numericalize([tokenized_sentence])
+
+# TODO pad out example sentence to maximum fixed sequence length
+
 # Save in ONNX format
-trainer.save_as_onnx("transformer.onnx")
+torch.onnx.export(model,                     # model being run
+                  src,                       # model input (or a tuple for multiple inputs)
+                  "transformer-pruned.onnx", # where to save the model (can be a file or file-like object)
+                  opset_version=12,          # the ONNX version to export the model to
+                  do_constant_folding=True,  # whether to execute constant folding for optimization
+                  input_names = ['src'],     # the model's input names
+                  output_names = ['output']) # the model's output names
 
