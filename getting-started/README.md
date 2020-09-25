c2fe6a1fefe53afa8223bede1391c28c176e6036
Nat Kershaw (MSFT) <nakersha@microsoft.com>
2020-09-25 16:55:05 -0700

    Update API for Getting Started sample (#42)

--- getting-started/README.md
+++ getting-started/README.md
@@ -6,8 +6,6 @@ This sample trains the model defined here: https://pytorch.org/tutorials/beginne
 
 Trained using the Wikitext-2 dataset (from `torchtext.dataset`), the language model assigns a probability for the likelihood of a given word (or a sequence of words) to follow a sequence of words.
 
-_Note: [ORTTrainer API](https://github.com/microsoft/onnxruntime/blob/orttraining_rc1/orttraining/orttraining/python/ort_trainer.py#L480) is experimental. A new version of the API is under active development, and this sample will be updated to that API soon._
-
 This sample uses the base docker image `mcr.microsoft.com/azureml/onnxruntime-training:0.1-rc2-openmpi4.0-cuda10.2-cudnn7.6-nccl2.7.6` to train the model. The image contains the ONNX Runtime package, OpenMP and NVIDIA GPU dependencies. The docker image can be used either standalone or in Azure Machine Learning.
 
 The purpose of this sample is to demonstrate the concepts and usage of ONNX Runtime Training, rather than to show its performance capabilities. For more complex samples that do demonstrate improved training performance at scale, please refer to the other samples in this repo:
@@ -111,7 +109,8 @@ This example runs on a single GPU. To configure distributed training, you can se
 2. Build the docker image
 
     ```bash
-    docker/build.sh docker
+    cd docker
+    bash build.sh 
     ```
 
     This will build a docker image called `ort-training-getting-started:latest`.
@@ -119,5 +118,5 @@ This example runs on a single GPU. To configure distributed training, you can se
 3. Launch the training code
 
     ```bash
-    docker/launch.sh "python train_ort.py"
+    bash docker/launch.sh "python train_ort.py"
     ```
--- getting-started/train_ort.py
+++ getting-started/train_ort.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import torchtext
 from torchtext.data.utils import get_tokenizer
 from model import TransformerModel
-from onnxruntime.capi.ort_trainer import IODescription, ModelDescription, ORTTrainer
+from onnxruntime.training import ORTTrainer, optim
 
 basic_english_tokenizer=get_tokenizer("basic_english")
 
@@ -37,17 +37,17 @@ def batchify(data, bsz):
     return data.to(device)
 
 batch_size = 20
-eval_batch_size = 20
+eval_batch_size = 10
 train_data = batchify(train_txt, batch_size)
 val_data = batchify(val_txt, eval_batch_size)
 test_data = batchify(test_txt, eval_batch_size)
 
 ntokens = len(TEXT.vocab.stoi) # the size of vocabulary
-emsize = 200     # embedding dimension
-nhid = 200       # the dimension of the feedforward network model in nn.TransformerEncoder
-nlayers = 2      # the number of nn.TransformerEncoderLayer in nn.TransformerEncoder
-nhead = 2        # the number of heads in the multiheadattention models
-dropout = 0.2    # the dropout value
+emsize = 200 # embedding dimension
+nhid = 200 # the dimension of the feedforward network model in nn.TransformerEncoder
+nlayers = 2 # the number of nn.TransformerEncoderLayer in nn.TransformerEncoder
+nhead = 2 # the number of heads in the multiheadattention models
+dropout = 0.2 # the dropout value
 
 model = TransformerModel(ntokens, emsize, nhead, nhid, nlayers, dropout).to(device)
 
@@ -57,18 +57,14 @@ def loss_with_flat_output(output, target):
     output = output.view(-1, ntokens)
     return criterion(output, target)
     
-lr = 0.001 # learning rate
+learning_rate = 0.1
 
 def train():
     total_loss = 0.
     start_time = time.time()
     for batch, i in enumerate(range(0, train_data.size(0) - 1, bptt)):
         data, targets = get_batch(train_data, i)
-        if len(data) < bptt:
-            print(f"len(data)={len(data)} < {bptt}")
-            continue
-        results = trainer.train_step(data, targets, torch.tensor([lr]))
-        loss = results[0]
+        loss, output = trainer.train_step(data, targets)
 
         total_loss += loss.item()
         log_interval = 200
@@ -76,9 +72,9 @@ def train():
             cur_loss = total_loss / log_interval
             elapsed = time.time() - start_time
             print('| epoch {:3d} | {:5d}/{:5d} batches | '
-                  'lr {:03.2f} | ms/batch {:5.2f} | '
+                  'lr {:02.3f} | ms/batch {:5.2f} | '
                   'loss {:5.2f} | ppl {:8.2f}'.format(
-                    epoch, batch, len(train_data) // bptt, lr,
+                    epoch, batch, len(train_data) // bptt, learning_rate,
                     elapsed * 1000 / log_interval,
                     loss, math.exp(loss)))
             total_loss = 0
@@ -89,11 +85,7 @@ def evaluate(eval_model, data_source):
     with torch.no_grad():
         for i in range(0, data_source.size(0) - 1, bptt):
             data, targets = get_batch(data_source, i)
-            if len(data) < bptt:
-                print(f"len(data)={len(data)} < {bptt}")
-                continue
-            results = trainer.eval_step(data, targets)
-            loss = results[0]
+            loss, outputs = trainer.eval_step(data, targets)
             total_loss += len(data) * loss.item()
     return total_loss / (len(data_source) - 1)
 
@@ -101,26 +93,17 @@ best_val_loss = float("inf")
 epochs = 3 # The number of epochs
 best_model = None
 
-def model_description():
-    input_desc = IODescription('src', [bptt, batch_size], torch.float32)
-    label_desc = IODescription('label', [bptt, batch_size, ntokens], torch.int64)
-    loss_desc = IODescription('loss', [], torch.float32)
-    output_desc = IODescription('output', [bptt, batch_size, ntokens], torch.float32)
-    return ModelDescription([input_desc, label_desc], [loss_desc, output_desc])
+model_description = {'inputs':  [('src', ['bptt', 'batch_size']),
+                                 ('label', ['bptt_x_batch_size'])],
+                     'outputs': [('loss', [], True),
+                                 ('output', ['bptt', 'batch_size', ntokens])]}
 
-def learning_rate_description():
-    return IODescription('Learning_Rate', [lr,], torch.float32)
-
+optimizer_config = optim.SGDConfig(lr=learning_rate)
 
 trainer = ORTTrainer(model,                       # model
-                     loss_with_flat_output,       # loss function
-                     model_description(),         # model description
-                     "SGDOptimizer",              # optimizer name
-                     None,                        # optimizer attributes
-                     learning_rate_description(), # learning rate description
-                     device,                      # device
-                     _opset_version=12)           # opset version
-
+                     model_description,           # model description
+                     optimizer_config,            # optimizer configuration
+                     loss_with_flat_output)       # loss function
 
 for epoch in range(1, epochs + 1):
     epoch_start_time = time.time()
