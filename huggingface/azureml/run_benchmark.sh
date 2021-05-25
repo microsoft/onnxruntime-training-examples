0679530732df92506847912d845ba67a901c6547
suffian khan <sukha@microsoft.com>
2021-05-25 16:20:04 -0700

    output to /logs

--- huggingface/azureml/run_benchmark.sh
+++ huggingface/azureml/run_benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo '' > /workspace/benchmark.txt
+echo '' > /logs/benchmark.txt
 
 for config in ort pt-fp16; do 
 for model in bert-large distilbert-base gpt2 bart-large t5-large deberta-v2-xxlarge roberta-large; do
@@ -15,11 +15,11 @@ for ngpu in 1 8; do
   command+=" --run_config $config --hf_model $model --gpu_cluster_name local --process_count $ngpu"
 
   echo $command
-  $command 2>&1 | tee /workspace/torch-$config-$model-$ngpu-gpu.txt
-  echo "torch-$config-$model-$ngpu-gpu.txt" >> benchmark.txt
+  $command 2>&1 | tee /logs/torch-$config-$model-$ngpu-gpu.txt
+  echo "torch-$config-$model-$ngpu-gpu.txt" >> /logs/benchmark.txt
   perf=`grep stable_train_samples_per_second\' /workspace/torch-$config-$model-$ngpu-gpu.txt`
-  echo $perf >> benchmark.txt
-  echo "" >> benchmark.txt
+  echo $perf >> /logs/benchmark.txt
+  echo "" >> /logs/benchmark.txt
   echo "" 
 
 done; done; done
