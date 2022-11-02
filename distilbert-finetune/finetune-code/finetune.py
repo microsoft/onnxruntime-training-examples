78f3c4507f1e4825fba7e600d75590689bfdb8d1
Prathik Rao <prathikrao@microsoft.com>
2022-11-02 12:16:49 -0700

    add code for youtube demo

---
+++ distilbert-finetune/finetune-code/finetune.py
@@ -0,0 +1,141 @@
+import argparse
+from pathlib import Path
+import os
+
+from azureml.core.run import Run
+from datasets import load_dataset
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer, TrainingArguments, Trainer, DefaultDataCollator
+
+def get_args(raw_args=None):
+    parser = argparse.ArgumentParser(description="DistilBERT Fine-Tuning")
+
+    parser.add_argument("--ort", action="store_true", help="Use ORTModule")
+    parser.add_argument("--deepspeed", action="store_true", help="Use deepspeed")
+
+    args = parser.parse_args(raw_args)
+    print(f"input parameters {vars(args)}")
+    return args
+
+def preprocess_function(examples, tokenizer=None):
+    questions = [q.strip() for q in examples["question"]]
+    inputs = tokenizer(
+        questions,
+        examples["context"],
+        max_length=384,
+        truncation="only_second",
+        return_offsets_mapping=True,
+        padding="max_length",
+    )
+
+    offset_mapping = inputs.pop("offset_mapping")
+    answers = examples["answers"]
+    start_positions = []
+    end_positions = []
+
+    for i, offset in enumerate(offset_mapping):
+        answer = answers[i]
+        start_char = answer["answer_start"][0]
+        end_char = answer["answer_start"][0] + len(answer["text"][0])
+        sequence_ids = inputs.sequence_ids(i)
+
+        # Find the start and end of the context
+        idx = 0
+        while sequence_ids[idx] != 1:
+            idx += 1
+        context_start = idx
+        while sequence_ids[idx] == 1:
+            idx += 1
+        context_end = idx - 1
+
+        # If the answer is not fully inside the context, label it (0, 0)
+        if offset[context_start][0] > end_char or offset[context_end][1] < start_char:
+            start_positions.append(0)
+            end_positions.append(0)
+        else:
+            # Otherwise it's the start and end token positions
+            idx = context_start
+            while idx <= context_end and offset[idx][0] <= start_char:
+                idx += 1
+            start_positions.append(idx - 1)
+
+            idx = context_end
+            while idx >= context_start and offset[idx][1] >= end_char:
+                idx -= 1
+            end_positions.append(idx + 1)
+
+    inputs["start_positions"] = start_positions
+    inputs["end_positions"] = end_positions
+    return inputs
+
+# NOTE: Training pipeline adapted from https://huggingface.co/docs/transformers/tasks/question_answering
+def main(raw_args=None):
+    args = get_args(raw_args)
+
+    # load the SQuAD dataset from the Huggingface Datasets library
+    squad = load_dataset("squad")
+
+    # load pretrained model and tokenizer
+    tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+    model = AutoModelForQuestionAnswering.from_pretrained("distilbert-base-uncased")
+
+    # documentation: https://github.com/pytorch/ort#add-onnx-runtime-for-pytorch-to-your-pytorch-training-script
+    if args.ort:
+        from onnxruntime.training import ORTModule
+        model = ORTModule(model)
+
+    # tokenize the data
+    tokenized_squad = squad.map(preprocess_function, fn_kwargs={"tokenizer": tokenizer}, batched=True, remove_columns=squad["train"].column_names)
+
+    # initialize training arguments
+    training_args_dict = {
+        "output_dir": ".outputs", # for intermediary checkpoints
+        "do_train": True,
+        "do_eval": True,
+        "per_device_train_batch_size": 16,
+        "per_device_eval_batch_size": 16,
+        "learning_rate": 2e-5,
+        "num_train_epochs": 50,
+        "weight_decay": 0.01,
+        "fp16": True,
+        "deepspeed": "ds_config_zero_1.json" if args.deepspeed else None,
+    }
+    training_args = TrainingArguments(**training_args_dict)
+
+    # Initialize Trainer
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=tokenized_squad["train"],
+        eval_dataset=tokenized_squad["validation"],
+        tokenizer=tokenizer,
+        # Data collator will default to DataCollatorWithPadding, so we change it.
+        data_collator=DefaultDataCollator(),
+    )
+
+    # train
+    train_result = trainer.train()
+
+    # extract performance metrics
+    train_metrics = train_result.metrics
+    train_metrics["train_samples"] = len(tokenized_squad["train"])
+    trainer.log_metrics("train", train_metrics)
+
+    eval_metrics = trainer.evaluate()
+    eval_metrics["eval_samples"] = len(tokenized_squad["validation"])
+    trainer.log_metrics("eval", eval_metrics)
+
+    rank = os.environ.get("RANK", -1)
+    if int(rank) == 0:
+        # save trained config, tokenizer and model
+        trained_model_folder = "model"
+        trained_model_path = Path(trained_model_folder)
+        trained_model_path.mkdir(parents=True, exist_ok=True)
+        model.save_pretrained(trained_model_path / "weights")
+
+        # upload saved data to AML
+        # documentation: https://learn.microsoft.com/en-us/python/api/azureml-core/azureml.core.run(class)?view=azure-ml-py
+        run = Run.get_context()
+        run.upload_folder(name="model", path=trained_model_folder)
+
+if __name__ == "__main__":
+    main()
---
+++ distilbert-finetune/inference.py
@@ -0,0 +1,97 @@
+import argparse
+import time
+import numpy as np
+
+import torch
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer
+import onnxruntime
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print("device: ", device)
+
+def infer(args):
+    # load model and update state dictionary
+    print("loading tokenizer and model...")
+    tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+    model = AutoModelForQuestionAnswering.from_pretrained("distilbert-base-uncased")
+    model.load_state_dict(torch.load("pytorch_model.bin"))
+    model.eval()
+
+    # load test data
+    context = """
+        Beyonce Giselle Knowles-Carter (born September 4, 1981) is an American singer, songwriter, record producer and actress.
+        Born and raised in Houston, Texas, she performed in various singing and dancing competitions as a child, and rose to fame
+        in the late 1990s as lead singer of R&B girl-group Destiny's Child. Managed by her father, Mathew Knowles, the group became
+        one of the world's best-selling girl groups of all time. Their hiatus saw the release of Beyoncé's debut album, Dangerously
+        in Love (2003), which established her as a solo artist worldwide, earned five Grammy Awards and featured the Billboard Hot
+        100 number-one singles 'Crazy in Love' and 'Baby Boy'.
+        """
+
+    questions = ["When was Beyonce born?", 
+                 "What areas did Beyonce compete in when she was growing up?", 
+                 "When did Beyonce leave Destiny's Child and become a solo singer?", 
+                 "What was the name of Beyonce's debut album?"]
+
+    # preprocess test data
+    inputs = []
+    for question in questions:
+        inputs.append((question, context))
+
+    # tokenize test data
+    print('tokenizing...')
+    encoding = tokenizer.batch_encode_plus(inputs, padding=True, return_tensors="pt")
+    input_ids, attention_mask = encoding["input_ids"], encoding["attention_mask"]
+
+    # if using onnnxruntime, convert to onnx format
+    # documentation: https://onnxruntime.ai/docs/api/python/api_summary.html
+    # ...more documentation: https://github.com/microsoft/onnxruntime/blob/main/onnxruntime/python/tools/transformers/notebooks/Inference_GPT2-OneStepSearch_OnnxRuntime_CPU.ipynb
+
+    if args.ort:
+        torch.onnx.export(model, \
+                          (input_ids, attention_mask), \
+                          "model.onnx", \
+                          input_names=['input_ids', 'attention_mask'], \
+                          output_names=['start_logits', "end_logits"]) 
+
+        sess = onnxruntime.InferenceSession('model.onnx', providers=['CUDAExecutionProvider'])
+        ort_input = {
+            'input_ids': np.ascontiguousarray(input_ids.numpy()),
+            'attention_mask' : np.ascontiguousarray(attention_mask.numpy()),
+        }
+
+    # run inference
+    start = time.time()
+    if args.ort:
+        output = sess.run(None, ort_input)
+    else:
+        output = model(input_ids, attention_mask=attention_mask)
+    end = time.time()
+
+    # postprocess test data
+    print("\n--------- RESULTS ---------")
+    print("Context: ", context)
+    for i in range(len(questions)):
+        if args.ort:
+            max_start_logits = output[0][i].argmax()
+            max_end_logits = output[1][i].argmax()
+        else:
+            max_start_logits = output.start_logits[i].argmax()
+            max_end_logits = output.end_logits[i].argmax()
+        ans_tokens = input_ids[i][max_start_logits: max_end_logits + 1]
+        answer_tokens = tokenizer.convert_ids_to_tokens(ans_tokens, skip_special_tokens=True)
+        answer_tokens_to_string = tokenizer.convert_tokens_to_string(answer_tokens)
+        print("Question: ", questions[i])
+        print("Answer: ", answer_tokens_to_string)
+
+    # brag about how fast we are
+    print("Inference time: ", end - start, "seconds")
+
+def main(raw_args=None):
+    parser = argparse.ArgumentParser(description="DistilBERT Fine-Tuning")
+    parser.add_argument("--ort", action='store_true', help="Use ORTModule")
+    args = parser.parse_args()
+
+    infer(args)
+
+if __name__ == "__main__":
+    main()
