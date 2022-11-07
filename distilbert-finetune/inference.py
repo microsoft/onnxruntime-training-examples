829d12c9adcdb38f90fb19bb12b116653c187916
Prathik Rao <prathikrao@microsoft.com>
2022-11-07 14:36:52 -0800

    trying io binding

--- distilbert-finetune/inference.py
+++ distilbert-finetune/inference.py
@@ -42,7 +42,7 @@ def infer(args):
 
     # load model and update state...
     model = AutoModelForQuestionAnswering.from_pretrained("distilbert-base-uncased")
-    model.load_state_dict(torch.load("pytorch_model.bin"))
+    model.load_state_dict(torch.load("pytorch_model.bin", map_location=torch.device(device)))
     model.eval()
 
     # if using onnnxruntime, convert to onnx format
@@ -58,55 +58,56 @@ def infer(args):
                             output_names=['start_logits', "end_logits"]) 
 
         sess = onnxruntime.InferenceSession('model.onnx', providers=['CUDAExecutionProvider'])
-        binding = sess.io_binding()
+        if device == "cuda":
+            binding = sess.io_binding()
 
-        input_ids_tensor = input_ids.contiguous()
-        attention_mask_tensor = attention_mask.contiguous()
+            input_ids_tensor = input_ids.contiguous()
+            attention_mask_tensor = attention_mask.contiguous()
 
-        binding.bind_input(
-            name='input_ids',
-            device_type='cuda',
-            device_id=0,
-            element_type=np.int64,
-            shape=tuple(input_ids_tensor.shape),
-            buffer_ptr=input_ids_tensor.data_ptr(),
-        )
+            binding.bind_input(
+                name='input_ids',
+                device_type='cuda',
+                device_id=0,
+                element_type=np.int64,
+                shape=tuple(input_ids_tensor.shape),
+                buffer_ptr=input_ids_tensor.data_ptr(),
+            )
 
-        binding.bind_input(
-            name='attention_mask',
-            device_type='cuda',
-            device_id=0,
-            element_type=np.int64,
-            shape=tuple(attention_mask_tensor.shape),
-            buffer_ptr=attention_mask_tensor.data_ptr(),
-        )
+            binding.bind_input(
+                name='attention_mask',
+                device_type='cuda',
+                device_id=0,
+                element_type=np.int64,
+                shape=tuple(attention_mask_tensor.shape),
+                buffer_ptr=attention_mask_tensor.data_ptr(),
+            )
 
-        start_logits_shape = (4, 164)
-        start_logits_tensor = torch.empty(start_logits_shape, dtype=torch.int64, device='cuda:0').contiguous()
-        binding.bind_output(
-            name='start_logits',
-            device_type='cuda',
-            device_id=0,
-            element_type=np.float32,
-            shape=tuple(start_logits_tensor.shape),
-            buffer_ptr=start_logits_tensor.data_ptr(),
-        )
+            start_logits_shape = (4, 164)
+            start_logits_tensor = torch.empty(start_logits_shape, dtype=torch.int64, device='cuda:0').contiguous()
+            binding.bind_output(
+                name='start_logits',
+                device_type='cuda',
+                device_id=0,
+                element_type=np.float32,
+                shape=tuple(start_logits_tensor.shape),
+                buffer_ptr=start_logits_tensor.data_ptr(),
+            )
 
-        end_logits_shape = (4, 164)
-        end_logits_tensor = torch.empty(end_logits_shape, dtype=torch.int64, device='cuda:0').contiguous()
-        binding.bind_output(
-            name='end_logits',
-            device_type='cuda',
-            device_id=0,
-            element_type=np.float32,
-            shape=tuple(end_logits_tensor.shape),
-            buffer_ptr=end_logits_tensor.data_ptr(),
-        )
-
-        # ort_input = {
-        #     'input_ids': np.ascontiguousarray(input_ids.numpy()),
-        #     'attention_mask' : np.ascontiguousarray(attention_mask.numpy()),
-        # }
+            end_logits_shape = (4, 164)
+            end_logits_tensor = torch.empty(end_logits_shape, dtype=torch.int64, device='cuda:0').contiguous()
+            binding.bind_output(
+                name='end_logits',
+                device_type='cuda',
+                device_id=0,
+                element_type=np.float32,
+                shape=tuple(end_logits_tensor.shape),
+                buffer_ptr=end_logits_tensor.data_ptr(),
+            )
+        elif device == "cpu":
+            ort_input = {
+                'input_ids': np.ascontiguousarray(input_ids.numpy()),
+                'attention_mask' : np.ascontiguousarray(attention_mask.numpy()),
+            }
 
     model.to(device)
     input_ids = input_ids.to(device)
@@ -119,11 +120,14 @@ def infer(args):
     for i in range(n_trials):
         start = time.time()
         if args.ort:
-            output = sess.run_with_iobinding(binding)
-            #output = sess.run(None, ort_input)
+            if device == "cuda":
+                output = sess.run_with_iobinding(binding)
+            elif device == "cpu":
+                output = sess.run(None, ort_input)
         else:
             output = model(input_ids, attention_mask=attention_mask)
         duration.append(time.time() - start)
+    average_inference_time = sum(duration) / n_trials
 
     # postprocess test data
     print("\n--------- RESULTS ---------")
@@ -131,10 +135,7 @@ def infer(args):
     for i in range(len(questions)):
         if args.ort:
             max_start_logits = output[0][i].argmax()
-            print("input_ids_shape", input_ids.shape)
-            print("start_logits_shape", output[0][i].shape)
             max_end_logits = output[1][i].argmax()
-            print("end_logits_shape", output[1][i].shape)
         else:
             max_start_logits = output.start_logits[i].argmax()
             max_end_logits = output.end_logits[i].argmax()
@@ -145,7 +146,7 @@ def infer(args):
         print("Answer: ", answer_tokens_to_string)
 
     # brag about how fast we are
-    print("Average Inference time: ", sum(duration)/n_trials, "seconds")
+    print("Average Inference Time:", average_inference_time, "seconds")
 
 def main(raw_args=None):
     parser = argparse.ArgumentParser(description="DistilBERT Fine-Tuning")
