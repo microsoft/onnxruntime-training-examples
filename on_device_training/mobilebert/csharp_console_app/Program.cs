4cbf1fc6153a9b7036b6ce1ce45fd58eb332f279
Caroline Zhu <carolinezhu@microsoft.com>
2023-04-07 13:36:53 -0400

    training loop in csharp demo

--- on_device_training/mobilebert/csharp_console_app/Program.cs
+++ on_device_training/mobilebert/csharp_console_app/Program.cs
@@ -9,59 +9,94 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            // Console.WriteLine("Hello, World!");
-            // TODO: wrap in try catch ???
-            // string test = Console.ReadLine();
-            // Console.WriteLine("You entered \"" + test + "\" in the console!");
             string parentDir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName;
-            string dataPath = Path.Combine(parentDir, "train_tokens.json");                   
-            DataLoader dl = new DataLoader();
+            string dataPath = Path.Combine(parentDir, "train_tokens.json");
 
-            DataSplit training = dl.ReadFile(dataPath);
-            Console.WriteLine("InputIds: " + training.InputIds.Length);
-            Console.WriteLine("InputShape: " + training.InputShape[0] + ", " + training.InputShape[1]);
-            Console.WriteLine("TokenTypeIds: " + training.TokenTypeIds);
-            Console.WriteLine("AttentionMask: " + training.AttentionMask);
-            Console.WriteLine("SpecialTokens: " + training.SpecialTokensMask.Length);
-             
-            CreateTrainingSession(training);
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            DataSplit ds = loadDataSplit(dataPath);
+
+            watch.Stop();
+
+            Console.WriteLine($"Execution Time for loading data split: {watch.ElapsedMilliseconds} ms");
+
+            TrainingSession ts = CreateTrainingSession();
+            trainEpoch(ts, ds, 4);
             Console.WriteLine("finished");
         }
 
-        public static void CreateTrainingSession(DataSplit ds)
+        public static DataSplit loadDataSplit(string dataPath)
         {
-            // TODO: change the strings from being hardcoded to ... making more sense
+            DataLoader dl = new DataLoader();
+            return dl.ReadFile(dataPath);
+        }
+
+        public static TrainingSession CreateTrainingSession()
+        {
             string parentDir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName;
             string checkpointPath = Path.Combine(parentDir, "training_artifacts", "mobilebert-uncased.ckpt");
             
             var state = new CheckpointState(checkpointPath);
             string trainingPath = Path.Combine(parentDir, "training_artifacts", "mobilebert-uncased_training.onnx");
-            var trainingSession = new TrainingSession(state, trainingPath);
+            string optimizerPath = Path.Combine(parentDir, "training_artifacts", "mobilebert-uncased_optimizer.onnx");
+            
+            return new TrainingSession(state, trainingPath, optimizerPath);
+        }
 
-            var memInfo = OrtMemoryInfo.DefaultInstance;
+        public static void trainEpoch(TrainingSession ts, DataSplit ds, int batchSize)
+        {
+            var numSamples = ds.InputShape[0];
+            var seqLen = ds.InputShape[1];
 
+            int steps = (int)(numSamples / batchSize);
+            // int steps = 20;
+            Console.WriteLine("steps: " + steps);
+            long[] inputShape = { batchSize, seqLen };
+
+            long startBatch = 0;
+            long endBatch = batchSize * seqLen;
+
+            for (int step = 0; step < steps; step++)
+            {
+                if (endBatch - startBatch < inputShape[0] * inputShape[1])
+                {
+                    inputShape[0] = (endBatch - startBatch) / seqLen;
+                }
+
+                // slices and creates inputs 
+                var inputs = createInputsFromDataSplit(ds, startBatch, endBatch, inputShape);
+
+                ts.LazyResetGrad();
+
+                var outputs = ts.TrainStep(inputs);
+
+                Console.WriteLine("Loss: " + outputs.ElementAtOrDefault(0).AsTensor<float>().GetValue(0));
+
+                ts.OptimizerStep();
+
+                startBatch += batchSize;
+                endBatch = Math.Min(endBatch + batchSize, (numSamples * seqLen) - 1);
+            }
+        }
+
+        public static List<FixedBufferOnnxValue> createInputsFromDataSplit(DataSplit ds, long start, long end, long[] shape)
+        {
             List<FixedBufferOnnxValue> inputs = new List<FixedBufferOnnxValue>();
+            inputs.Add(sliceInt64ArrayToBufferOnnx(ds.InputIds, start, end, shape));
+            inputs.Add(sliceInt64ArrayToBufferOnnx(ds.AttentionMask, start, end, shape));
+            inputs.Add(sliceInt64ArrayToBufferOnnx(ds.TokenTypeIds, start, end, shape));
+            inputs.Add(sliceInt64ArrayToBufferOnnx(ds.Labels, start, end, shape));
+            return inputs;
+        }
 
-            // order of inputs should follow same order as the inputs for your onnx model
-            FixedBufferOnnxValue inputIds = FixedBufferOnnxValue.CreateFromMemory<Int64>(memInfo, 
-                ds.InputIds, Microsoft.ML.OnnxRuntime.Tensors.TensorElementType.Int64, 
-                ds.InputShape, ds.InputIds.Length * sizeof(Int64));
+        public static FixedBufferOnnxValue sliceInt64ArrayToBufferOnnx(Int64[] array, long start, 
+            long end, long[] shape)
+        {
+            var memInfo = OrtMemoryInfo.DefaultInstance;
+            Int64[] arraySlice = array[(int)start..(int)end];
 
-            inputs.Add(inputIds);
-
-            FixedBufferOnnxValue attentionMask = FixedBufferOnnxValue.CreateFromMemory<Int32>(memInfo,
-                ds.AttentionMask, Microsoft.ML.OnnxRuntime.Tensors.TensorElementType.Int32,
-                ds.AttentionShape, ds.AttentionMask.Length * sizeof(Int32));
-
-            inputs.Add(attentionMask);
-
-            FixedBufferOnnxValue tokenTypeIds = FixedBufferOnnxValue.CreateFromMemory<Int32>(memInfo,
-                ds.TokenTypeIds, Microsoft.ML.OnnxRuntime.Tensors.TensorElementType.Int32,
-                ds.TokenShape, ds.TokenTypeIds.Length * sizeof(Int32));
-
-            inputs.Add(tokenTypeIds);
-
-            trainingSession.TrainStep(inputs);
+            return FixedBufferOnnxValue.CreateFromMemory<Int64>(memInfo, arraySlice,
+                Microsoft.ML.OnnxRuntime.Tensors.TensorElementType.Int64, shape,
+                arraySlice.Length * sizeof(Int64));
         }
     }
 }
