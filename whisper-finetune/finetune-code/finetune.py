71f789cbcaed27a4937999364e76eec8f2b9403c
Prathik Rao <prathik.rao@gmail.com>
2023-04-21 23:54:28 -0700

    adjust whisper batch size (#109)

--- whisper-finetune/finetune-code/finetune.py
+++ whisper-finetune/finetune-code/finetune.py
@@ -94,8 +94,8 @@ def finetune(args):
         output_dir="output_dir",
         do_train=True,
         do_eval=True,
-        per_device_train_batch_size=2,
-        per_device_eval_batch_size=2,
+        per_device_train_batch_size=1,
+        per_device_eval_batch_size=1,
         fp16=True,
         num_train_epochs=50,
         deepspeed="ds_config_zero_1.json" if args.ort_ds else None
