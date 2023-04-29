68ae4e63c77120086e268046073557942cecc698
Prathik Rao <prathik.rao@gmail.com>
2023-04-28 17:11:44 -0700

    finetune works! (#118)

--- whisper-finetune/aml_submit.py
+++ whisper-finetune/aml_submit.py
@@ -20,9 +20,7 @@ def get_args(raw_args=None):
     # accelerator hyperparameters
     parser.add_argument("--ort_ds", action="store_true", help="Use ORTModule and DeepSpeed to accelerate training")
 
-    parser.add_argument("--torch_version", choices=["1.13", "2.0"], default="1.13", help="Specify PyTorch version")
-
-    parser.add_argument("--nebula", action="store_true", help="Enable nebula checkpointing")
+    parser.add_argument("--torch_version", choices=["1.13", "2.0"], default="2.0", help="Specify PyTorch version")
 
     # parse args, extra_args used for job configuration
     args = parser.parse_args(raw_args)
@@ -45,6 +43,11 @@ def main(raw_args=None):
     # tags
     tags = vars(args)
 
+    if args.ort_ds:
+        command_str = "torchrun --nproc-per-node 8 run_speech_recognition_seq2seq_ort.py --model_name_or_path=openai/whisper-small --dataset_name=mozilla-foundation/common_voice_11_0 --dataset_config_name=hi --language=hindi --output_dir=output_dir --per_device_train_batch_size=8 --per_device_eval_batch_size=8 --num_train_epochs 100 --preprocessing_num_workers=16 --length_column_name=input_length --max_duration_in_seconds=30 --text_column_name=sentence --freeze_feature_encoder=False --group_by_length --fp16 --do_train --do_eval --predict_with_generate --optim adamw_ort_fused --deepspeed zero_stage_1.json"
+    else:
+        command_str = "torchrun --nproc-per-node 8 run_speech_recognition_seq2seq.py --model_name_or_path=openai/whisper-small --dataset_name=mozilla-foundation/common_voice_11_0 --dataset_config_name=hi --language=hindi --output_dir=output_dir --per_device_train_batch_size=8 --per_device_eval_batch_size=8 --num_train_epochs 100 --preprocessing_num_workers=16 --length_column_name=input_length --max_duration_in_seconds=30 --text_column_name=sentence --freeze_feature_encoder=False --group_by_length --fp16 --do_train --do_eval --predict_with_generate"
+
     # define the command
     # documentation: https://learn.microsoft.com/en-us/python/api/azure-ai-ml/azure.ai.ml.entities.command?view=azure-python
     command_job = command(
@@ -52,13 +55,12 @@ def main(raw_args=None):
         display_name="whisper-finetune"+ ("-ort-ds" if args.ort_ds else ""),
         experiment_name="acpt-whisper-finetune-demo",
         code=code_dir,
-        command=(
-            "torchrun --nproc_per_node=8 finetune.py " + ("--ort_ds " if args.ort_ds else " ") + (" --nebula " if args.nebula else " ")
-        ),
+        command=command_str,
         environment="acpt-whisper-torch{0}@latest".format(args.torch_version.replace(".", "")),
         compute=args.compute,
         instance_count=1,
         tags=tags,
+        shm_size="16g",
     )
 
     # submit the command
--- whisper-finetune/inference.py
+++ whisper-finetune/inference.py
@@ -2,57 +2,17 @@ import argparse
 from datasets import Audio, DatasetDict, load_dataset
 import time
 import torch
-from transformers import WhisperForConditionalGeneration, WhisperProcessor
+from transformers import AutoModelForSpeechSeq2Seq, WhisperProcessor
 from transformers import GenerationMixin
+from transformers import pipeline
 
 from torch.utils.data import DataLoader
 from typing import Any, Dict, List, Union
 from dataclasses import dataclass
+import numpy as np
+
 
 processor = WhisperProcessor.from_pretrained("openai/whisper-small", language="Hindi", task="transcribe")
-
-@dataclass
-class DataCollatorSpeechSeq2SeqWithPadding:
-    """
-    Data collator that will dynamically pad the inputs received.
-    Args:
-        processor ([`WhisperProcessor`])
-            The processor used for processing the data.
-        decoder_start_token_id (`int`)
-            The begin-of-sentence of the decoder.
-        forward_attention_mask (`bool`)
-            Whether to return attention_mask.
-    """
-
-    processor: Any
-    decoder_start_token_id: int
-    forward_attention_mask: bool
-
-    def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
-        # split inputs and labels since they have to be of different lengths and need
-        # different padding methods
-        model_input_name = self.processor.model_input_names[0]
-        input_features = [{model_input_name: feature[model_input_name]} for feature in features]
-        label_features = [{"input_ids": feature["labels"]} for feature in features]
-
-        batch = self.processor.feature_extractor.pad(input_features, return_tensors="pt")
-
-        if self.forward_attention_mask:
-            batch["attention_mask"] = torch.LongTensor([feature["attention_mask"] for feature in features])
-
-        labels_batch = self.processor.tokenizer.pad(label_features, return_tensors="pt")
-
-        # replace padding with -100 to ignore loss correctly
-        labels = labels_batch["input_ids"].masked_fill(labels_batch.attention_mask.ne(1), -100)
-
-        # if bos token is appended in previous tokenization step,
-        # cut bos token here as it's append later anyways
-        if (labels[:, 0] == self.decoder_start_token_id).all().cpu().item():
-            labels = labels[:, 1:]
-
-        batch["labels"] = labels
-
-        return batch
 
 def prepare_dataset(batch):
     # load and resample audio data from 48 to 16kHz
@@ -71,23 +31,36 @@ def infer(args):
     common_voice = DatasetDict()
 
     common_voice["test"] = load_dataset("mozilla-foundation/common_voice_11_0", "hi", split="test")
-    common_voice = common_voice.remove_columns(["accent", "age", "client_id", "down_votes", "gender", "locale", "path", "segment", "up_votes"])
+    # common_voice = common_voice.remove_columns(["accent", "age", "client_id", "down_votes", "gender", "locale", "path", "segment", "up_votes"])
     common_voice = common_voice.cast_column("audio", Audio(sampling_rate=16000))
 
-    common_voice = common_voice.map(prepare_dataset, remove_columns=common_voice.column_names["test"], num_proc=4)
+    # common_voice = common_voice.map(prepare_dataset, remove_columns=common_voice.column_names["test"], num_proc=4)
 
-    model = WhisperForConditionalGeneration.from_pretrained("output_dir/checkpoint-2000")
-    model.config.forced_decoder_ids = None
-    model.config.suppress_tokens = []
+    model = AutoModelForSpeechSeq2Seq.from_pretrained("openai/whisper-small")
+    model.load_state_dict(torch.load("pytorch_model.bin", map_location=torch.device(device)), strict=False) # maybe try strict=False??
+    # model.config.forced_decoder_ids = None
+    # model.config.suppress_tokens = []
     model.eval()
 
-    collate_fn = DataCollatorSpeechSeq2SeqWithPadding(processor=processor, decoder_start_token_id=model.config.decoder_start_token_id, forward_attention_mask=False)
-    dataloader = DataLoader(common_voice["test"], collate_fn=collate_fn, batch_size=4)
+    print(common_voice["test"][0])
+    inputs = processor(common_voice["test"][0]["audio"]["array"], return_tensors="pt")
+    input_features = inputs.input_features
 
-    if not args.ort:
-        inputs = processor(common_voice["audio"][0]["array"], return_tensors="pt")
-        input_features = inputs.input_features
-    # decoder_input_ids = torch.tensor([[1, 1]]) * model.config.decoder_start_token_id
+    # if using onnnxruntime, convert to onnx format
+    # ORT Python API Documentation: https://onnxruntime.ai/docs/api/python/api_summary.html
+    if args.ort:
+        if not os.path.exists("model.onnx"):
+            torch.onnx.export(model, \
+                            (input_ids, attention_mask), \
+                            "model.onnx", \
+                            input_names=["input_ids", "attention_mask"], \
+                            output_names=["start_logits", "end_logits"]) 
+
+        sess = onnxruntime.InferenceSession("model.onnx", providers=["CUDAExecutionProvider", "CPUExecutionProvider"])
+        ort_input = {
+                "input_ids": np.ascontiguousarray(input_ids.numpy()),
+                "attention_mask" : np.ascontiguousarray(attention_mask.numpy()),
+            }
 
     # if using onnnxruntime, convert to onnx format
     # ORT Python API Documentation: https://onnxruntime.ai/docs/api/python/api_summary.html
@@ -95,27 +68,15 @@ def infer(args):
         from torch_ort import ORTModule
         model = ORTModule(model)
 
-    # input_features = input_features.to(device)
-    # decoder_input_ids = decoder_input_ids.to(device)
+        # input_features = np.ascontiguousarray(input_features)
+
+    input_features = input_features.to(device)
     model.to(device)
 
     # run inference
     print("running inference...")
     start = time.time()
-    if args.ort:
-        # NOTE: this copies data from CPU to GPU
-        # since our data is small, we are still faster than baseline pytorch
-        # refer to ORT Python API Documentation for information on io_binding to explicitly move data to GPU ahead of time
-        for batch in dataloader:
-            print(batch)
-            decoder_input_ids = torch.tensor([[1, 1]]) * model.config.decoder_start_token_id
-            decoder_input_ids = decoder_input_ids.to(device)
-            input_features = batch["input_features"].to(device)
-            generated_ids = model(input_features, decoder_input_ids=decoder_input_ids).logits 
-        # print(output)
-        # generated_ids = GenerationMixin.greedy_search(output)
-    else:
-        generated_ids = model.generate(inputs=input_features)
+    generated_ids = model.generate(inputs=input_features)
     end = time.time()
 
     print("generated_ids", generated_ids)
@@ -124,6 +85,7 @@ def infer(args):
 
     # brag about how fast we are
     print("Inference Time:", str(end - start), "seconds")
+
 
 def main():
     parser = argparse.ArgumentParser(description="Whisper Fine-Tuning")
