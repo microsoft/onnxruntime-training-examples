d3bd50dfe9bee0eac99b1722517f6144a4f7a4d0
Prathik Rao <prathik.rao@gmail.com>
2023-05-04 20:57:50 +0000

    add client inference code

---
+++ whisper-finetune/inference_endpoint.py
@@ -0,0 +1,47 @@
+import urllib.request
+import json
+import os
+import ssl
+import librosa
+
+def allowSelfSignedHttps(allowed):
+    # bypass the server certificate verification on client side
+    if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
+        ssl._create_default_https_context = ssl._create_unverified_context
+
+# allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
+
+# Request data goes here
+# The example below assumes JSON formatting which may be updated
+# depending on the format your endpoint expects.
+# More information can be found here:
+# https://docs.microsoft.com/azure/machine-learning/how-to-deploy-advanced-entry-script
+audio = librosa.load("common_voice_hi_23795238.mp3")[0]
+audio = audio.tolist()
+data = {"audio": audio}
+
+body = str.encode(json.dumps(data))
+
+url = 'https://acpt-ort-whisper.eastus.inference.ml.azure.com/score'
+# Replace this with the primary/secondary key or AMLToken for the endpoint
+api_key = 'aLHyiVXY1uv7PgZ3FLutFmYtPiBoBOnh'
+if not api_key:
+    raise Exception("A key should be provided to invoke the endpoint")
+
+# The azureml-model-deployment header will force the request to go to a specific deployment.
+# Remove this header to have the request observe the endpoint traffic rules
+headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key), 'azureml-model-deployment': 'whisper-onnx-1' }
+
+req = urllib.request.Request(url, body, headers)
+
+try:
+    response = urllib.request.urlopen(req)
+
+    result = response.read()
+    print(result)
+except urllib.error.HTTPError as error:
+    print("The request failed with status code: " + str(error.code))
+
+    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+    print(error.info())
+    print(error.read().decode("utf8", 'ignore'))
