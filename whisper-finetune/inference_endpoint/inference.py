f3bda20069d2009bd8d5e1ef4997612e25f10053
Prathik Rao <prathik.rao@gmail.com>
2023-05-05 23:45:38 +0000

    Adjust inference

--- whisper-finetune/inference_endpoint/inference.py
+++ whisper-finetune/inference_endpoint/inference.py
@@ -22,7 +22,7 @@ if not api_key:
 
 # The azureml-model-deployment header will force the request to go to a specific deployment.
 # Remove this header to have the request observe the endpoint traffic rules
-headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key), 'azureml-model-deployment': 'acpt-ort-whisper-1' }
+headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key), 'azureml-model-deployment': '{endpoint_deployment_name}' }
 
 req = urllib.request.Request(url, body, headers)
 
