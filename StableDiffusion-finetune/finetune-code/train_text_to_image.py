75e56de37daeea6ce50261d9d115102bda078c76
Prathik Rao <prathik.rao@gmail.com>
2023-06-22 16:22:20 -0700

    Update train_text_to_image.py

--- StableDiffusion-finetune/finetune-code/train_text_to_image.py
+++ StableDiffusion-finetune/finetune-code/train_text_to_image.py
@@ -885,10 +885,7 @@ def main():
                     raise ValueError(f"Unknown prediction type {noise_scheduler.config.prediction_type}")
 
                 # Predict the noise residual and compute loss
-                if args.ort:
-                    model_pred = unet(noisy_latents, timesteps, encoder_hidden_states, return_dict=False)[0]
-                else:
-                    model_pred = unet(noisy_latents, timesteps, encoder_hidden_states).sample
+                model_pred = unet(noisy_latents, timesteps, encoder_hidden_states).sample
 
                 if args.snr_gamma is None:
                     loss = F.mse_loss(model_pred.float(), target.float(), reduction="mean")
