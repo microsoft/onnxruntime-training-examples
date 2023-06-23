06eeb76d8aebada6517ca6770ba6f1c0f092d958
root <root@orttrainingdev8.d32nl1ml4oruzj4qz3bqlggovf.px.internal.cloudapp.net>
2023-06-23 23:12:21 +0000

    add back mlflow logic

--- T5/src/Finetune/train_summarization_deepspeed_optum.py
+++ T5/src/Finetune/train_summarization_deepspeed_optum.py
@@ -51,7 +51,7 @@ import mlflow
 import subprocess
 import pickle
 import torch
-# import azureml.evaluate.mlflow as azmlflow
+import azureml.evaluate.mlflow as azmlflow
 import torch.profiler.profiler as profiler
 
 import datasets
@@ -607,40 +607,40 @@ def main():
             
             subprocess.call(["python", "-m", "onnxruntime.transformers.convert_generation", "-m", "t5-small", "--model_type", "t5", "--decoder_onnx", str(trained_model_path / "onnx" / "outputs_decoder.onnx"), "--encoder_decoder_init_onnx", str(trained_model_path / "onnx" / "outputs_encoder_decoder_init.onnx"), "--output", str(trained_model_path / "onnx" / "outputs_beam_search.onnx")])
             
-            # conda_env = {
-            #     'channels': ['conda-forge'],
-            #     'dependencies': [
-            #         'python=3.8.8',
-            #         'pip',
-            #         {'pip': [
-            #         'mlflow',
-            #         'torch==2.0.0',
-            #         'transformers',
-            #         'azureml-evaluate-mlflow'
-            #         ]}
-            #     ],
-            #     'name': 'mlflow-env'
-            # }
-            # misc_conf = {
-            #     "task_type": "summarization"
-            # }
+            conda_env = {
+                'channels': ['conda-forge'],
+                'dependencies': [
+                    'python=3.8.8',
+                    'pip',
+                    {'pip': [
+                    'mlflow',
+                    'torch==2.0.0',
+                    'transformers',
+                    'azureml-evaluate-mlflow'
+                    ]}
+                ],
+                'name': 'mlflow-env'
+            }
+            misc_conf = {
+                "task_type": "summarization"
+            }
             
-            # components = {
-            #     "model": model,
-            #     "tokenizer": tokenizer,
-            #     "config": config,
-            # }
+            components = {
+                "model": model,
+                "tokenizer": tokenizer,
+                "config": config,
+            }
             
             
-            #Saving the Model as MLFlow model
+            # Saving the Model as MLFlow model
 
-            # model_info = azmlflow.hftransformers.save_model(
-            #     hf_model=model,
-            #     tokenizer=tokenizer,
-            #     config=config,
-            #     path=os.path.join(str(trained_model_path), "mlflow"), conda_env=conda_env,
-            #     hf_conf=misc_conf
-            # )
+            model_info = azmlflow.hftransformers.save_model(
+                hf_model=model,
+                tokenizer=tokenizer,
+                config=config,
+                path=os.path.join(str(trained_model_path), "mlflow"), conda_env=conda_env,
+                hf_conf=misc_conf
+            )
        
 
     # Evaluation
