c215eabe8153cf5b3b01b4b10ba1db85b51c3296
Prathik Rao <prathik.rao@gmail.com>
2023-06-26 13:08:49 -0700

    bug fixes to T5 demo (#150)
    Co-authored-by: root <root@orttrainingdev8.d32nl1ml4oruzj4qz3bqlggovf.px.internal.cloudapp.net>

--- T5/src/Finetune/train_summarization_deepspeed_optum.py
+++ T5/src/Finetune/train_summarization_deepspeed_optum.py
@@ -603,7 +603,7 @@ def main():
             # Converting the pytorch model to onnx format
             # Documentation for encoder-decoder export: https://github.com/microsoft/onnxruntime/blob/main/onnxruntime/python/tools/transformers/models/t5/convert_to_onnx.py
             # Documentation for general export: https://pytorch.org/docs/stable/onnx.html#example-alexnet-from-pytorch-to-onnx
-            subprocess.call(["python", "-m", "onnxruntime.transformers.models.t5.convert_to_onnx", "-m", "t5-small", "--output", str(trained_model_path / "onnx"), "--use_external_data_format", "--state_dict_path", "pytorch_model.bin"])
+            subprocess.call(["python", "-m", "onnxruntime.transformers.models.t5.convert_to_onnx", "-m", str(trained_model_path), "--output", str(trained_model_path / "onnx"), "--use_external_data_format"])
             
             subprocess.call(["python", "-m", "onnxruntime.transformers.convert_generation", "-m", "t5-small", "--model_type", "t5", "--decoder_onnx", str(trained_model_path / "onnx" / "outputs_decoder.onnx"), "--encoder_decoder_init_onnx", str(trained_model_path / "onnx" / "outputs_encoder_decoder_init.onnx"), "--output", str(trained_model_path / "onnx" / "outputs_beam_search.onnx")])
             
@@ -632,7 +632,7 @@ def main():
             }
             
             
-            #Saving the Model as MLFlow model
+            # Saving the Model as MLFlow model
 
             model_info = azmlflow.hftransformers.save_model(
                 hf_model=model,
